daaff679b72b03883b1124daf0faf05cbc201a5a
AlexeyKarasev <karasev.alexey@gmail.com>
2018-12-09 02:25:40 +0300

    Updates

---
+++ precache-manifest.4291a907ab4607598d70232bc5fd8ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51e204425e55d9747c4f",
+    "url": "/erc20/static/css/main.640d8a99.chunk.css"
+  },
+  {
+    "revision": "51e204425e55d9747c4f",
+    "url": "/erc20/static/js/main.51e20442.chunk.js"
+  },
+  {
+    "revision": "1b030395a43bcaf77066",
+    "url": "/erc20/static/js/1.1b030395.chunk.js"
+  },
+  {
+    "revision": "2bdeb145f91a02513d83",
+    "url": "/erc20/static/js/runtime~main.2bdeb145.js"
+  },
+  {
+    "revision": "7bea37299167a86f83c398f7204576fb",
+    "url": "/erc20/index.html"
+  }
+];
